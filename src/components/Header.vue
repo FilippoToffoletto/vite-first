2b080cbd8d8678577abad0e49811f37058236b50
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-14 16:58:18 +0100

    [add]Header & Main  text

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name:'PrimoComponente'
+}
+</script>
+
+<template>
+    <h1 class="main-title">
+      La mia prima app con Vue
+    </h1>
+    <h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ut quas enim molestiae aliquid omnis accusamus ipsa. Fuga, labore minus.
+    </h3>
+</template>
+
+<style>
+
+
+h1,h3{
+  text-align: center;
+  max-width: 60%;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+
+.main-title{
+  color: #ef476f;
+  font-size: 3rem;
+}
+
+h3{
+  color: #ffd166;
+  font-weight: 400;
+}
+</style>
---
+++ src/components/MainText.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name:'MainText'
+}
+</script>
+
+<template>
+  <div class="main-text">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic quis animi doloribus reprehenderit illum libero aut accusantium neque molestias repellat excepturi voluptatibus adipisci similique delectus porro dolorem facilis ea magni? Dolor incidunt numquam commodi, dolorum amet facere perferendis quam ut ad natus, quas dolores soluta et eveniet corporis vitae.
+    </p>
+  </div>
+</template>
+
+
+
+<style>
+.main-text{
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 20px;
+  color:#f77f00 ;
+}
+
+</style>
